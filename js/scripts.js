59e4b19cd74979d3069553398add55acf2e98f03
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-26 20:01:53 -0800

    add prototype method to assign incremental unique ID's to each added contact when called

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-function addressBook () {
+// Business Logic for AddressBook ---------
+function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
-
+AddressBook.prototype.addContact = function (contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.firstName] = contact;
+};
+AddressBook.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
-  this.lastName = lastName; 
+  this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 Contact.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName; 
+  return this.firstName + " " + this.lastName;
 };
