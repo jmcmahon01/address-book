f7d7d881e3d77d27880f33089943c0d865b57743
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-26 20:15:32 -0800

    add prototype method findContact to retrieve contacts by ID

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,30 @@ function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
+
 AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
-  this.contacts[contact.firstName] = contact;
+  this.contacts[contact.id] = contact;
 };
+
 AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 };
+AddressBook.prototype.findContact = function (id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
+
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 };
